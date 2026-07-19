36109dddce486d23ed5bc0b336ef810b76e56b00
VardhanShah-09 <shahvardhan28@gmail.com>
2026-07-19 12:26:56 +0530

    Refactor dashboard architecture and optimize model loading

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
 import streamlit as st
 
+from components.Dashboard import show as dashboard
+from components.Stock_Analysis import show as stock_analysis
+from components.Prediction import show as prediction
+from components.Comparison import show as comparison
+from components.About import show as about
+
 st.set_page_config(
     page_title="Nifty500 Dashboard",
     page_icon="📈",
     layout="wide"
 )
+
+tab1, tab2, tab3, tab4, tab5 = st.tabs([
+    "📊 Dashboard",
+    "📈 Stock Analysis",
+    "🤖 Prediction",
+    "⚖️ Comparison",
+    "ℹ️ About"
+])
+
+with tab1:
+    dashboard()
+
+with tab2:
+    stock_analysis()
+
+with tab3:
+    prediction()
+
+with tab4:
+    comparison()
+
+with tab5:
+    about()
 
 st.title("Nifty500 Stock Market Dashboard")
 
--- utils/loader.py
+++ utils/loader.py
@@ -1,11 +1,20 @@
 import streamlit as st
 import pandas as pd
 from pathlib import Path
+import joblib
+import sys
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATA_PATH = BASE_DIR / "Data" / "Processed_data" / "cleaned_nifty500.csv"
 FEATURES_PATH = BASE_DIR / "Data" / "Processed_data" / "features_nifty500.csv"
+
+ROOT = Path(__file__).resolve().parent.parent
+sys.path.append(str(ROOT))
+
+# -----------------------
+# Load Dataset
+# -----------------------
 
 @st.cache_data
 def load_data():
@@ -14,9 +23,21 @@ def load_data():
     df = df.sort_values("Date")
     return df
 
+
 @st.cache_data
 def load_features():
     df = pd.read_csv(FEATURES_PATH)
     df["Date"] = pd.to_datetime(df["Date"])
     df = df.sort_values("Date")
     return df
+
+
+# -----------------------
+# Load ML Model
+# -----------------------
+
+MODEL_PATH = ROOT / "models" / "random_forest.pkl"
+
+@st.cache_resource
+def load_model():
+    return joblib.load(MODEL_PATH)
